2ab723370f75f4c4b6484bced808d0c48e696da4
ニャット <kuniharu_japan@yahoo.co.jp>
2026-02-26 03:19:33 +0900

    Update vocal.js

--- vocal.js
+++ vocal.js
@@ -80,7 +80,7 @@ async function main(){
 
     if(songsNote){
       songsNote.textContent = repSongs.length
-        ? "代表曲を表示中"
+        ? ""
         : "代表曲未設定のため、最新曲を表示中"
     }
 
@@ -117,7 +117,7 @@ async function main(){
 
     if(popularNote){
       popularNote.textContent = hasScore
-        ? "人気曲を表示中"
+        ? ""
         : "人気データ未集計のため、現在は新着順"
     }
 
